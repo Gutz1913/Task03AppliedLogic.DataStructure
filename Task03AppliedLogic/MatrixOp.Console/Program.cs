59c9269e6fb1945e4e6ed22c88838885b93ddc8f
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-16 16:49:56 -0500

    Task 03 Applied Logic Completed.

---
+++ Task03AppliedLogic/MatrixOp.Console/Program.cs
@@ -0,0 +1,53 @@
+﻿using Shared;
+
+var response = string.Empty;
+do
+{
+    try
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Clear();
+        var n = ConsoleExtension.GetInt("Ingrese orden de la matriz: ");
+        int[,] matrix = new int[n, n];
+        int sum = 0;
+        int max = int.MinValue;
+        int min = int.MaxValue;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                int value = (i + 1) - j;
+                matrix[i, j] = value;
+                sum += value;
+
+                if (value > max) max = value;
+                if (value < min) min = value;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrix[i, j].ToString().PadRight(6));
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine($"La sumatoriaes: {sum}");
+        Console.WriteLine($"El valor máximo es: {max}");
+        Console.WriteLine($"El valor mínimo es: {min}");
+        Console.WriteLine();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    Console.Write("¿Desea continuar? (S/N): ");
+    response = Console.ReadLine()!.ToUpper();
+    Console.WriteLine();
+} while (response == "S");
+
+Console.WriteLine("\n :::::::::: GAME OVER :::::::::: \n");
---
+++ Task03AppliedLogic/PrimeFactors.Console/Program.cs
@@ -0,0 +1,70 @@
+﻿using Shared;
+
+var response = string.Empty;
+do
+{
+    try
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Clear();
+        var number = ConsoleExtension.GetInt("Ingrese el número a descomponer: ");
+
+        List<int> factors = GetPrimeFactors(number);
+
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.Black;
+
+        Console.Write($"{number} = ");
+
+        for (int i = 0; i < factors.Count; i++)
+        {
+            Console.Write(factors[i]);
+            if (i < factors.Count - 1)
+            {
+                Console.Write(" x ");
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.Black;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    Console.Write("¿Desea continuar? (S/N): ");
+    response = Console.ReadLine()!.ToUpper();
+    Console.WriteLine();
+} while (response == "S");
+
+Console.WriteLine("\n :::::::::: GAME OVER :::::::::: \n");
+
+List<int> GetPrimeFactors(int number)
+{
+    List<int> factors = new List<int>();
+
+    while (number % 2 == 0)
+    {
+        factors.Add(2);
+        number /= 2;
+    }
+
+    for (int i = 3; i * i <= number; i += 2)
+    {
+        while (number % i == 0)
+        {
+            factors.Add(i);
+            number /= i;
+        }
+    }
+
+    if (number > 2)
+    {
+        factors.Add(number);
+    }
+    return factors;
+}
